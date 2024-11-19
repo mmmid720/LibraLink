78679e5dcc0ddb4dbd3bc9771247f513f15efb50
mmmid720 <mmmid720@gmail.com>
2024-11-19 19:04:50 +0900

    js->jsx, logo create

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const path = require('path');
-const cors = require('cors');
 const app = express();
 
-// CORS 설정
-app.use(cors());
+// 몽고DB 연결
+connectDB();
 
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname, '../frontend/build')));
@@ -13,10 +14,6 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) => {
     res.sendFile(path.jopin(__dirname, '../frontend/src/App.js'));
 })
-
-// JSON 데이터 파싱
-app.use(express.json());
-
 
 app.listen(8080, () => {
     console.log('서버 시작중... 포트번호: 8080');
